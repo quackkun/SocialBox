250cbf47e27dcc6bc71b61440e47d44bafe16b8d
quackkun <74012138+quackkun@users.noreply.github.com>
2020-11-06 01:33:58 +0900

    Create install-sb.sh

---
+++ install-sb.sh
@@ -0,0 +1,30 @@
+＃！/ bin / bash
+＃ Sysupdate
+sudo apt-get update 
+＃パイソン
+sudo apt-get install python
+＃ Gmailの
+git clone https://github.com/Ha3MrX/Gemail-Hack.git
+＃フェイスブック
+if [ -d facebook] ;  その後
+        echo  "ディレクトリfacebookが存在します。[✔] "
+そうしないと
+	mkdir facebook
+	cd facebook
+	wget raw.githubusercontent.com/Sup3r-Us3r/scripts/master/fb-brute.pl
+	cd ..
+fi
+＃ Instagramの
+git clone https://github.com/thelinuxchoice/instainsane.git
+cd instainsane
+chmod + x instainsane.sh
+chmod + x install.sh
+sudo ./install.sh
+cd ..
+＃ツイッター
+git clone https://github.com/thelinuxchoice/tweetshell.git
+cdツイートシェル
+chmod + xtweetshell.sh
+chmod + x install.sh
+sudo ./install.sh
+cd ..
